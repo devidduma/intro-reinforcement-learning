19264457620553f4dbe5a015ff3c6a40b99e7408
Devid Duma <devidd05@gmail.com>
2020-08-19 14:03:42 +0200

    implemented first visit, every visit and incremental MC

--- 1-grid-world/3-monte-carlo/mc_agent.py
+++ 1-grid-world/3-monte-carlo/mc_agent.py
@@ -4,17 +4,24 @@ from collections import defaultdict
 from environment import Env
 
 
+class VisitState:
+    def __init__(self, name, total_G, N, V):
+        self.name = name
+        self.total_G = total_G
+        self.N = N
+        self.V = V
+
 # Monte Carlo Agent which learns every episodes from the sample
 class MCAgent:
     def __init__(self, actions):
         self.width = 5
         self.height = 5
         self.actions = actions
-        self.learning_rate = 0.01
         self.discount_factor = 0.9
         self.epsilon = 0.1
         self.samples = []
         self.value_table = defaultdict(float)
+        self.visit_state = []
 
     # append sample to memory(state, reward, done)
     def save_sample(self, state, reward, done):
@@ -22,18 +29,65 @@ class MCAgent:
 
     # for every episode, agent updates q function of visited states
     def update(self):
-        G_t = 0
-        visit_state = []
+        # state name and G for each state as appeared in the episode
+        all_states = []
+        G = 0
         for reward in reversed(self.samples):
-            state = str(reward[0])
-            if state not in visit_state:
-                visit_state.append(state)
-                G_t = self.discount_factor * (reward[1] + G_t)
-                value = self.value_table[state]
-                self.value_table[state] = (value +
-                                           self.learning_rate * (G_t - value))
+            state_name = str(reward[0])
+            G = reward[1] + self.discount_factor * G
+            all_states.append([state_name, G])
+        all_states.reverse()
 
-    # get action for the state according to the q function table
+        # use either first visit, every visit or incremental MC
+        self.first_visit_mc(all_states)
+
+    def first_visit_mc(self, all_states):
+        visit_state = []
+        for state in all_states:
+            if state[0] not in visit_state:
+                visit_state.append(state[0])
+                self.update_global_visit_state(state[0], state[1])
+        for state in self.visit_state:
+            self.value_table[state.name] = state.V
+
+    def every_visit_mc(self, all_states):
+        for state in all_states:
+                self.update_global_visit_state(state[0], state[1])
+        for state in self.visit_state:
+            self.value_table[state.name] = state.V
+
+    def incremental_mc(self, all_states):
+        for state in all_states:
+            self.update_global_visit_state(state[0], state[1], incremental=True)
+        for state in self.visit_state:
+            self.value_table[state.name] = state.V
+
+    def update_global_visit_state(self, state_name, G_t, incremental = False):
+        if not incremental:
+            updated = False
+            for vs in self.visit_state:
+                if vs.name == state_name:
+                    vs.total_G = vs.total_G + G_t
+                    vs.N = vs.N + 1
+                    vs.V = vs.total_G / vs.N
+                    updated = True
+                    break
+            if not updated:
+                self.visit_state.append(VisitState(name=state_name, total_G=G_t, N=1, V=G_t))
+        else:
+            updated = False
+            for vs in self.visit_state:
+                if vs.name == state_name:
+                    vs.N = vs.N + 1
+                    learning_rate = 0.5 * 1 / vs.N
+                    vs.V = vs.V + learning_rate * (G_t - vs.V)
+                    updated = True
+                    break
+            if not updated:
+                self.visit_state.append(VisitState(name=state_name, total_G=None, N=1, V=G_t))
+
+
+    # get action for the state according to the v function table
     # agent pick action of epsilon-greedy policy
     def get_action(self, state):
         if np.random.rand() < self.epsilon:
@@ -103,7 +157,7 @@ if __name__ == "__main__":
             # get next action
             action = agent.get_action(next_state)
 
-            # at the end of each episode, update the q function table
+            # at the end of each episode, update the v function table
             if done:
                 print("episode : ", episode)
                 agent.update()
