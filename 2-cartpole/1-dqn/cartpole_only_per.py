24e8197467da49124653137725ffab30a88ef42b
Devid Duma <devidd05@gmail.com>
2020-08-27 17:40:51 +0200

    fixed model hidden layer size, translated dqn_per file comments

--- 2-cartpole/1-dqn/cartpole_only_per.py
+++ 2-cartpole/1-dqn/cartpole_only_per.py
@@ -32,20 +32,21 @@ class DQNAgent:
         self.train_start = 2000
         self.memory_size = 2000
 
-        # 리플레이 메모리, 최대 크기 2000
+        # create replay memory using deque
         self.memory = Memory(self.memory_size)
 
-        # 모델과 타깃 모델 생성
+        # create main model and target model
         self.model = self.build_model()
         self.target_model = self.build_model()
 
-        # 타깃 모델 초기화
+        # initialize target model
         self.update_target_model()
 
         if self.load_model:
             self.model.load_weights("./save_model/cartpole_dqn_trained.h5")
 
-    # 상태가 입력, 큐함수가 출력인 인공신경망 생성
+    # approximate Q function using Neural Network
+    # state is input and Q Value of each action is output of network
     def build_model(self):
         model = Sequential()
         model.add(Dense(24, input_dim=self.state_size, activation='relu',
@@ -58,11 +59,11 @@ class DQNAgent:
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
 
-    # 타깃 모델을 모델의 가중치로 업데이트
+    # after some time interval update the target model to be same with model
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
 
-    # 입실론 탐욕 정책으로 행동 선택
+    # get action from model using epsilon-greedy policy
     def get_action(self, state):
         if np.random.rand() <= self.epsilon:
             return random.randrange(self.action_size)
@@ -70,12 +71,12 @@ class DQNAgent:
             q_value = self.model.predict(state)
             return np.argmax(q_value[0])
 
-    # 샘플 <s, a, r, s'>을 리플레이 메모리에 저장
+    # save sample <TD-error, s,a,r,s'> to the replay memory
     def append_sample(self, state, action, reward, next_state, done):
         if self.epsilon == 1:
             done = True
 
-        # TD-error 를 구해서 같이 메모리에 저장
+        # TD-error
         target = self.model.predict([state])
         old_val = target[0][action]
         target_val = self.target_model.predict([next_state])
@@ -88,12 +89,11 @@ class DQNAgent:
 
         self.memory.add(error, (state, action, reward, next_state, done))
 
-    # 리플레이 메모리에서 무작위로 추출한 배치로 모델 학습
+    # pick samples randomly from replay memory (with batch_size)
     def train_model(self):
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
-        # 메모리에서 배치 크기만큼 무작위로 샘플 추출
         mini_batch = self.memory.sample(self.batch_size)
 
         errors = np.zeros(self.batch_size)
@@ -108,23 +108,21 @@ class DQNAgent:
             next_states[i] = mini_batch[i][1][3]
             dones.append(mini_batch[i][1][4])
 
-        # 현재 상태에 대한 모델의 큐함수
-        # 다음 상태에 대한 타깃 모델의 큐함수
         target = self.model.predict(states)
         target_val = self.target_model.predict(next_states)
 
-        # 벨만 최적 방정식을 이용한 업데이트 타깃
         for i in range(self.batch_size):
             old_val = target[i][actions[i]]
+            # Q Learning: get maximum Q value at s' from target model
             if dones[i]:
                 target[i][actions[i]] = rewards[i]
             else:
                 target[i][actions[i]] = rewards[i] + self.discount_factor * (
                     np.amax(target_val[i]))
-            # TD-error를 저장
+            # TD-error
             errors[i] = abs(old_val - target[i][actions[i]])
 
-        # TD-error로 priority 업데이트
+        # TD-error priority
         for i in range(self.batch_size):
             idx = mini_batch[i][0]
             self.memory.update(idx, errors[i])
@@ -167,12 +165,13 @@ class Memory:  # stored as ( s, a, r, s_ ) in SumTree
 
 
 if __name__ == "__main__":
-    # CartPole-v1 환경, 최대 타임스텝 수가 500
+    # In case of CartPole-v1, maximum length of episode is 500
     env = gym.make('CartPole-v1')
+    # get size of state and action from environment
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
 
-    # DQN 에이전트 생성
+    # DQN Agent
     agent = DQNAgent(state_size, action_size)
 
     scores, episodes = [], []
@@ -181,7 +180,6 @@ if __name__ == "__main__":
     for e in range(EPISODES):
         done = False
         score = 0
-        # env 초기화
         state = env.reset()
         state = np.reshape(state, [1, state_size])
 
@@ -189,16 +187,16 @@ if __name__ == "__main__":
             if agent.render:
                 env.render()
             step += 1
-            # 현재 상태로 행동을 선택
+
+            # get action for the current state and go one step in environment
             action = agent.get_action(state)
-            # 선택한 행동으로 환경에서 한 타임스텝 진행
             next_state, reward, done, info = env.step(action)
             next_state = np.reshape(next_state, [1, state_size])
-            # 에피소드가 중간에 끝나면 -100 보상
+
             r = reward if not done or score+reward == 500 else -10
-            # 리플레이 메모리에 샘플 <s, a, r, s'> 저장
+            # save the sample <s, a, r, s'> to the replay memory
             agent.append_sample(state, action, r, next_state, done)
-            # 매 타임스텝마다 학습
+
             if step >= agent.train_start:
                 agent.train_model()
 
