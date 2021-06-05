2be585ea0c5938f7a6cb27f011856fb81d8fdaee
Devid Duma <devidd05@gmail.com>
2021-06-05 19:59:40 +0200

    improved SARSA and documented Jupyter Notebook for SARSA

--- 1-grid-world/6-sarsa/sarsa_agent.py
+++ 1-grid-world/6-sarsa/sarsa_agent.py
@@ -9,10 +9,11 @@ from environment import Env
 class SARSAgent:
     def __init__(self, actions):
         self.actions = actions
-        self.learning_rate = 0.4
+        self.learning_rate = 1
         self.discount_factor = 0.9
         self.decaying_epsilon_counter = 1
-        self.decaying_epsilon_mul_factor = 0.2
+        self.decaying_epsilon_mul_factor = 0.1
+        self.epsilon = None
         self.q_table = defaultdict(lambda: [0.0, 0.0, 0.0, 0.0])
 
     # with sample <s, a, r, s', a'>, learns new q function
@@ -27,8 +28,7 @@ class SARSAgent:
     # get action for the state according to the q function table
     # agent pick action of epsilon-greedy policy
     def get_action(self, state):
-        epsilon = 1 / (self.decaying_epsilon_counter * self.decaying_epsilon_mul_factor)
-        if np.random.rand() < epsilon:
+        if np.random.rand() < self.epsilon:
             # take random action
             action = np.random.choice(self.actions)
         else:
@@ -36,6 +36,16 @@ class SARSAgent:
             state_action = self.q_table[state]
             action = self.arg_max(state_action)
         return action
+
+    # epsilon-greedy policy
+    def update_epsilon(self):
+        self.epsilon = 1 / (self.decaying_epsilon_counter * self.decaying_epsilon_mul_factor)
+
+    # decaying learning rate satisfying Robbins-Munro sequence
+    def update_learning_rate(self):
+        self.learning_rate = 1 / (self.decaying_epsilon_counter * self.decaying_epsilon_mul_factor)
+        if self.learning_rate > 1:
+            self.learning_rate = 1
 
     @staticmethod
     def arg_max(state_action):
@@ -50,37 +60,49 @@ class SARSAgent:
                 max_index_list.append(index)
         return random.choice(max_index_list)
 
+    def mainloop(self, env, verbose = False):
+        for episode in range(1000):
+            # reset environment and initialize state
+            state = env.reset()
+
+            # update epsilon and get action of state from agent
+            self.update_epsilon()
+            action = self.get_action(str(state))
+
+            while True:
+                env.render()
+
+                # take action and proceed one step in the environment
+                next_state, reward, done = env.step(action)
+
+                # update epsilon and get next action
+                self.update_epsilon()
+                next_action = self.get_action(str(next_state))
+
+                # with sample <s,a,r,s',a'>, agent learns new q function
+                self.learn(str(state), action, reward, str(next_state), next_action)
+
+                state = next_state
+                action = next_action
+
+                # print q function of all states at screen
+                env.print_value_all(self.q_table)
+
+                # if episode ends, then break
+                if done:
+                    self.decaying_epsilon_counter = self.decaying_epsilon_counter + 1
+                    # decaying learning rate satisfying Robbins-Munro sequence
+                    self.update_learning_rate()
+
+                    if verbose:
+                        print("episode: ", episode,
+                              "\tepsilon: ", round(self.epsilon, 2),
+                              "\tlearning rate: ", round(self.learning_rate, 2)
+                              )
+                    break
+
+
 if __name__ == "__main__":
     env = Env()
     agent = SARSAgent(actions=list(range(env.n_actions)))
-
-    for episode in range(1000):
-        # reset environment and initialize state
-
-        state = env.reset()
-        # get action of state from agent
-        action = agent.get_action(str(state))
-
-        while True:
-            env.render()
-
-            # take action and proceed one step in the environment
-            next_state, reward, done = env.step(action)
-            next_action = agent.get_action(str(next_state))
-
-            # with sample <s,a,r,s',a'>, agent learns new q function
-            agent.learn(str(state), action, reward, str(next_state), next_action)
-
-            state = next_state
-            action = next_action
-
-            # print q function of all states at screen
-            env.print_value_all(agent.q_table)
-
-            # if episode ends, then break
-            if done:
-                agent.decaying_epsilon_counter = agent.decaying_epsilon_counter + 1
-
-                print("episode : ", episode)
-                break
-
+    agent.mainloop(env, verbose=True)
